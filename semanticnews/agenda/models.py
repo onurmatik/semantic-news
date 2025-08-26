5603c42d9e3ae3fc1fbf469a41aec73cfe4784c7
onur mat <onurmatik@gmail.com>
2025-08-26 07:23:39 +0300

    Added event significance

--- semanticnews/agenda/models.py
+++ semanticnews/agenda/models.py
@@ -18,6 +18,14 @@ class Event(models.Model):
 
     locality = models.ForeignKey('agenda.Locality', on_delete=models.CASCADE, blank=True, null=True)
     categories = models.ManyToManyField('agenda.Category', blank=True)
+    significance = models.PositiveSmallIntegerField(choices=(
+        (1, 'Very low'),
+        (2, 'Low'),
+        (3, 'Normal'),
+        (4, 'High'),
+        (5, 'Very high'),
+    ), default=4)
+
     status = models.CharField(max_length=20, choices=(
         ('draft', 'Draft'),
         ('published', 'Published'),
