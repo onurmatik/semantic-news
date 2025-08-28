14f6debce0e2128ce78d991724a20e96046b3c79
Onur Mat <onurmatik@gmail.com>
2025-08-28 16:02:36 +0300

    Display related and suggested agenda events on topic page

--- semanticnews/topics/views.py
+++ semanticnews/topics/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render, get_object_or_404
+from pgvector.django import L2Distance
+
+from semanticnews.agenda.models import Event
 from .models import Topic
 
 
 def topics_detail(request, slug, username):
     topic = get_object_or_404(
-        Topic, slug=slug,
+        Topic.objects.prefetch_related("events"),
+        slug=slug,
         created_by__username=username,
     )
-    return render(request, 'topics/topics_detail.html', {
-        'topic': topic,
-    })
+
+    related_events = topic.events.all()
+
+    if topic.embedding is not None:
+        suggested_events = (
+            Event.objects.exclude(topics=topic)
+            .exclude(embedding__isnull=True)
+            .annotate(distance=L2Distance("embedding", topic.embedding))
+            .order_by("distance")[:5]
+        )
+    else:
+        suggested_events = Event.objects.none()
+
+    return render(
+        request,
+        "topics/topics_detail.html",
+        {
+            "topic": topic,
+            "related_events": related_events,
+            "suggested_events": suggested_events,
+        },
+    )
