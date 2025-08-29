55d4d92d6ba93db21030ba8ed7203e929c6a259a
onur mat <onurmatik@gmail.com>
2025-08-29 09:28:55 +0300

    Updated logger settings

--- semanticnews/settings.py
+++ semanticnews/settings.py
@@ -169,6 +169,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGGING = {
     "version": 1,
+    "disable_existing_loggers": False,
     "handlers": {"console": {"class": "logging.StreamHandler"}},
     "loggers": {
         "semanticnews.openai": {"handlers": ["console"], "level": "DEBUG"},
