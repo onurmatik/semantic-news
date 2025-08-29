7f0ab37d66e7f9824ddb50b195773f236af439cf
onur mat <onurmatik@gmail.com>
2025-08-29 07:07:04 +0300

    Admin updates

--- semanticnews/topics/admin.py
+++ semanticnews/topics/admin.py
@@ -1,6 +1,7 @@
 from asgiref.sync import async_to_sync
 from django.contrib import admin
 from .models import Topic, TopicContent
+from .utils.recaps import admin as recaps_admin  # noqa: F401
 
 
 @admin.register(Topic)
--- semanticnews/topics/utils/recaps/admin.py
+++ semanticnews/topics/utils/recaps/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TopicRecap
+
+
+@admin.register(TopicRecap)
+class TopicRecapAdmin(admin.ModelAdmin):
+    list_display = ("topic", "created_at", "short_recap")
+    search_fields = ("topic__title", "recap")
+    readonly_fields = ("created_at",)
+
+    def short_recap(self, obj):
+        return obj.recap[:50] + ("..." if len(obj.recap) > 50 else "")
+    short_recap.short_description = "recap"
