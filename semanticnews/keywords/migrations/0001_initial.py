d8c4d786a583804f6af7c922c0193625c43294dd
Onur Mat <onurmatik@gmail.com>
2025-09-04 21:32:15 +0300

    Add keyword and topickeyword migrations

---
+++ semanticnews/keywords/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    initial = True
+
+    dependencies = []
+
+    operations = [
+        migrations.CreateModel(
+            name='Keyword',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=100)),
+                ('slug', models.CharField(blank=True, max_length=100, unique=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('variant_type', models.CharField(blank=True, choices=[('synonym', 'Synonym'), ('hypernym', 'Hypernym'), ('hyponym', 'Hyponym'), ('abbreviation', 'Abbreviation'), ('ignore', 'Ignore')], max_length=100, null=True)),
+                ('variant_of', models.ForeignKey(blank=True, null=True, on_delete=models.CASCADE, to='keywords.keyword')),
+            ],
+        ),
+    ]
