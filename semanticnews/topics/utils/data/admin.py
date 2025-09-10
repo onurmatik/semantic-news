538277c3ab8f343e718541c5aa33081abc2b2400
onur mat <onurmatik@gmail.com>
2025-09-10 22:15:02 +0300

    Added TopicDataVisualization admin

--- semanticnews/topics/utils/data/admin.py
+++ semanticnews/topics/utils/data/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import TopicData, TopicDataInsight
+from .models import TopicData, TopicDataInsight, TopicDataVisualization
 
 
 @admin.register(TopicData)
@@ -14,4 +14,11 @@ class TopicDataAdmin(admin.ModelAdmin):
 class TopicDataInsightAdmin(admin.ModelAdmin):
     list_display = ("topic", "insight")
     search_fields = ("topic", "insight")
+    readonly_fields = ("created_at",)
+
+
+@admin.register(TopicDataVisualization)
+class TopicDataVisualizationAdmin(admin.ModelAdmin):
+    list_display = ("topic", "insight", "chart_type")
+    list_filter = ("chart_type",)
     readonly_fields = ("created_at",)
