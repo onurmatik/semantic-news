8881d1141f7ba35262d652247a72fda7fa45a497
onur mat <onurmatik@gmail.com>
2025-09-10 20:23:30 +0300

    Added DataInsightAdmin

--- semanticnews/topics/utils/data/admin.py
+++ semanticnews/topics/utils/data/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
-from .models import TopicData
+from .models import TopicData, TopicDataInsight
 
 
 @admin.register(TopicData)
 class TopicDataAdmin(admin.ModelAdmin):
     list_display = ("topic", "name", "url", "created_at")
     search_fields = ("topic__title", "name", "url")
+    readonly_fields = ("created_at",)
+
+
+@admin.register(TopicDataInsight)
+class TopicDataInsightAdmin(admin.ModelAdmin):
+    list_display = ("topic", "insight")
+    search_fields = ("topic", "insight")
     readonly_fields = ("created_at",)
