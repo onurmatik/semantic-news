9c587f760df47111de0909ea1d16c0e1331806f6
onur mat <onurmatik@gmail.com>
2025-10-24 09:41:27 +0300

    Removed utils folder

--- semanticnews/topics/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Legacy namespace for topic utility modules.
-
-The former ``semanticnews.topics.utils`` apps now live under
-``semanticnews.widgets``. Imports should be updated to use the new
-package path.
-"""
