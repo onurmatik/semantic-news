f02c4eb8998955acabd518990a505a6e81511d28
onur mat <onurmatik@gmail.com>
2025-10-25 22:03:13 +0300

    Removed arguments widget

--- semanticnews/widgets/arguments/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ArgumentsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'semanticnews.widgets.arguments'
-    app_label = 'topics'
