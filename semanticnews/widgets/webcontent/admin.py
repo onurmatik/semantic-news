c3a8c9790ac2c0d42b0b2f1ad97095dd8cf0b5dd
Onur Mat <onurmatik@gmail.com>
2025-10-25 22:00:33 +0300

    Merge document and embed widgets into webcontent app

--- semanticnews/widgets/webcontent/admin.py
+++ semanticnews/widgets/webcontent/admin.py
@@ -1,8 +1,8 @@
-"""Admin configuration for topic document and webpage links."""
+"""Admin registrations for web content widgets."""
 
 from django.contrib import admin
 
-from .models import TopicDocument, TopicWebpage
+from .models import TopicDocument, TopicTweet, TopicWebpage, TopicYoutubeVideo
 
 
 @admin.register(TopicDocument)
@@ -34,3 +34,15 @@ class TopicWebpageAdmin(admin.ModelAdmin):
     search_fields = ('title', 'topic__title', 'url')
     readonly_fields = ('created_at',)
     raw_id_fields = ('topic', 'created_by')
+
+
+@admin.register(TopicTweet)
+class TopicTweetAdmin(admin.ModelAdmin):
+    list_display = ('topic', 'tweet_id', 'url', 'created_at')
+    search_fields = ('url',)
+
+
+@admin.register(TopicYoutubeVideo)
+class TopicYoutubeVideoAdmin(admin.ModelAdmin):
+    list_display = ('topic', 'title', 'video_id', 'status', 'created_at')
+    search_fields = ('title', 'video_id')
---
+++ semanticnews/widgets/webcontent/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WebContentConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'semanticnews.widgets.webcontent'
+    verbose_name = 'Topic web content'
